0736afb475dc0ebe4271dc6e98e7f29d42bb74e7
dinesh sankariya <dinesh.dina47@gmail.com>
2020-10-09 21:53:39 +0530

    new commit

--- src/main/java/com/dinesh/heroku/HerokuApplication.java
+++ src/main/java/com/dinesh/heroku/HerokuApplication.java
@@ -1,7 +1,11 @@
 package com.dinesh.heroku;
 
+import com.dinesh.heroku.Model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class HerokuApplication {
@@ -9,5 +13,17 @@ public class HerokuApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuApplication.class, args);
 	}
+
+}
+
+@Controller
+class HomenewController {
+
+	@RequestMapping("/")
+	public String home_page(Model model){
+		model.addAttribute("user",new User());
+		return "/home/page";
+	}
+
 
 }
