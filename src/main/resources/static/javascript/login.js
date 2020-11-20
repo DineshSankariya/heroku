c9901a309a56de5c0033dcc26409d72b4c7f182e
dinesh sankariya <dinesh.dina47@gmail.com>
2020-11-20 11:14:01 +0530

    new commit on 11:13 ::  20/11/2020

---
+++ src/main/resources/static/javascript/login.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    $(".submit").click(function(event){
+
+        event.preventDefault();
+
+        var name=$(".email").val();
+        var pass=$(".password").val();
+
+        if(name && pass){
+
+            $.ajax({
+
+                        type:"GET",
+                        url:"/user-api/loggingprocess",
+                        data:{
+                            "username":name,
+                            "password":pass
+                        },
+                        success:function(data){
+
+                            if(data=="granted"){
+
+                                   window.location.href="/home/success";
+
+                            }else{
+//                                alert("Invalid credential");
+                                 var error="<p>User doesn't exists</p>";
+                                    $("h2").append(error);
+                                    $("p").css({"margin":"0px","padding":"0px","font-size":"19px"});
+//                                    alert("Enter valid username and password");
+                                    setTimeout(function(){
+                                        window.location.href="/home/login";
+                                    },2500);
+                            }
+
+                        }
+
+                    })
+
+
+        }else{
+            alert("Credential should not be empty..");
+        }
+
+
+    })
+
+
+})
